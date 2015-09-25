119a4f07755cad3af11d634319b735e80c1410fb
Patrik Kullman <patrik.kullman@gmail.com>
2015-09-25 22:44:36 +0200

    Initial commit

---
+++ linter.py
@@ -0,0 +1,36 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Patrik,,,
+# Copyright (c) 2015 Patrik,,,
+#
+# License: MIT
+#
+
+"""This module exports the Polylint plugin class."""
+
+from SublimeLinter.lint import NodeLinter, util
+
+
+class Polylint(NodeLinter):
+
+    """Provides an interface to polylint."""
+
+    syntax = ('html')
+    cmd = 'polylint --only-inputs -i'
+    executable = None
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0'
+    regex = r'^[^:]*:(?P<line>\d+):(?P<col>\d+)\r?\n\s*(?P<message>.+)$'
+    multiline = True
+    line_col_base = (1, 1)
+    tempfile_suffix = '-'
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
+    inline_settings = None
+    inline_overrides = None
+    comment_re = r'\s*/[/*]'
